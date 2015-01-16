30ae7e2fb5d10f8aa23a59eab9821c93c33165c6
danielsf <scott.f.daniel@gmail.com>
2015-01-16 12:22:01 -0800

    again, hotwire TMV

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -1,12 +1,13 @@
-export SCONSFLAGS=$SCONSFLAGS" USE_UNKNOWN_VARS=true TMV_DIR="$TMV_DIR" PREFIX="$PREFIX" EXTRA_LIB_PATH="$TMV_DIR"/lib EXTRA_INCLUDE_PATH="$PREFIX"/include"
+export SFDPREFIX=/astro/users/danielsf/lsstsw5/lsstsw/stack/Linux64/tmv/tickets.DM-1763-g2e109ef5d1
+export SCONSFLAGS=$SCONSFLAGS" USE_UNKNOWN_VARS=true TMV_DIR="$TMV_DIR" PREFIX="$PREFIX" EXTRA_LIB_PATH="$SFD"/lib EXTRA_INCLUDE_PATH="$SFDPREFIX"/include"
 
 build(){
     echo 'SFD TMV_DIR'
     echo $TMV_DIR
     echo 'SFD DYLD_LIBRARY_PATH'
     echo $DYLD_LIBRARY_PATH
-    echo 'PREFIX'
-    echo $PREFIX
+    echo 'SFDPREFIX'
+    echo $SFDPREFIX
 
     default_build
 }
