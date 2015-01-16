96678480b10a57ac839dc4f1262c5a177f6876ea
danielsf <scott.f.daniel@gmail.com>
2015-01-16 12:53:18 -0800

    remove diagnostic print statements

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -1,10 +1,1 @@
 export SCONSFLAGS=$SCONSFLAGS" USE_UNKNOWN_VARS=true TMV_DIR="$TMV_DIR" PREFIX="$PREFIX" EXTRA_LIB_PATH="$TMV_DIR"/lib EXTRA_INCLUDE_PATH="$TMV_DIR"/include"
-
-build(){
-    echo 'SFD TMV_DIR'
-    echo $TMV_DIR
-    echo 'SFD DYLD_LIBRARY_PATH'
-    echo $DYLD_LIBRARY_PATH
-
-    default_build
-}
