f4e435e9e3ffdb062c45be129ee95f844de6e1a2
Jim Bosch <jbosch@astro.princeton.edu>
2015-02-19 15:37:55 -0500

    Override eupspkg install to copy header files

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -1,1 +1,7 @@
 export SCONSFLAGS=$SCONSFLAGS" USE_UNKNOWN_VARS=true TMV_DIR="$TMV_DIR" PREFIX="$PREFIX" PYPREFIX="$PREFIX"/lib/python EXTRA_LIB_PATH="$TMV_DIR"/lib EXTRA_INCLUDE_PATH="$TMV_DIR"/include"
+
+install()
+{
+    default_install
+    cp -r include $PREFIX/
+}
