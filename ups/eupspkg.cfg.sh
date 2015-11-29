f7aa84c13acf96dcbe9308140e8b494ffd095550
danielsf <scott.f.daniel@gmail.com>
2015-11-28 17:51:01 -0800

    oops

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -15,7 +15,7 @@ build(){
     declare -a arr
     arr=( $(otool -L $BOOST_DIR/lib/libboost_python.dylib) )
     echo 'sfd id'
-    init_id=arr[1]
+    init_id=${arr[1]}
     echo $init_id
     #install_name_tool -id $BOOST_DIR/lib/libboost_python.dylib $BOOST_DIR/lib/libboost_python.dylib
     default_build
