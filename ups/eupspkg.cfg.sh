45dca64820618ca0805512349ab3f1001ed7dd85
danielsf <scott.f.daniel@gmail.com>
2015-11-29 13:36:59 -0800

    broke up really long SCONSFLAGS assignment line

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -1,4 +1,6 @@
-export SCONSFLAGS=$SCONSFLAGS" USE_UNKNOWN_VARS=true TMV_DIR="$TMV_DIR" PREFIX="$PREFIX" PYPREFIX="$PREFIX"/lib/python EXTRA_LIB_PATH="$TMV_DIR"/lib EXTRA_INCLUDE_PATH="$TMV_DIR"/include"
+export SCONSFLAGS=$SCONSFLAGS" USE_UNKNOWN_VARS=true TMV_DIR="$TMV_DIR
+" PREFIX="$PREFIX" PYPREFIX="$PREFIX"/lib/python EXTRA_LIB_PATH="$TMV_DIR
+"/lib EXTRA_INCLUDE_PATH="$TMV_DIR"/include"
 
 pathToPython=$(which python)
 pathToPythonLib="${pathToPython%bin/python}/lib"
