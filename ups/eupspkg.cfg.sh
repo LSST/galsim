8b73eb9873ea7cac67a616dbfb399c0037926147
danielsf <scott.f.daniel@gmail.com>
2015-11-30 10:53:11 -0800

    spelling error

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -4,7 +4,7 @@ pathToPythonLib="${pathToPython%bin/python}/lib"
 export SCONSFLAGS=$SCONSFLAGS" USE_UNKNOWN_VARS=true TMV_DIR="$TMV_DIR\
 " PREFIX="$PREFIX" PYPREFIX="$PREFIX"/lib/python EXTRA_LIB_PATH="$TMV_DIR\
 "/lib EXTRA_INCLUDE_PATH="$TMV_DIR"/include"\
-" LSST_LIBRARY_PATH="$patyToPythonLib:$LSST_LIBRARY_PATH
+" LSST_LIBRARY_PATH="$pathToPythonLib:$LSST_LIBRARY_PATH
 
 install()
 {
