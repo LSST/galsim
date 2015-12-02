2a05678be9bab3c82c80b2f2aa942d85cd684912
danielsf <scott.f.daniel@gmail.com>
2015-12-01 21:54:05 -0800

    that was a bad idea

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -4,7 +4,7 @@ pathToPythonLib="${pathToPython%bin/python}/lib"
 export SCONSFLAGS=$SCONSFLAGS" USE_UNKNOWN_VARS=true TMV_DIR="$TMV_DIR\
 " PREFIX="$PREFIX" PYPREFIX="$PREFIX"/lib/python EXTRA_LIB_PATH="$TMV_DIR\
 "/lib EXTRA_INCLUDE_PATH="$TMV_DIR"/include"\
-" FFTW_DIR="$FFTW_DIR"/lib"\
+" FFTW_DIR="$FFTW_DIR\
 " FALLBACK="$pathToPythonLib\
 " LINKFLAGS=-Wl,-rpath="$pathToPythonLib
 
