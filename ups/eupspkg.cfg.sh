f9b6869cfb3c06b480e68b24fc5ad54b2963923b
danielsf <scott.f.daniel@gmail.com>
2015-12-01 21:33:41 -0800

    typo in LINKFLAGS

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -5,7 +5,7 @@ export SCONSFLAGS=$SCONSFLAGS" USE_UNKNOWN_VARS=true TMV_DIR="$TMV_DIR\
 " PREFIX="$PREFIX" PYPREFIX="$PREFIX"/lib/python EXTRA_LIB_PATH="$TMV_DIR\
 "/lib EXTRA_INCLUDE_PATH="$TMV_DIR"/include"\
 " FALLBACK="$pathToPythonLib\
-" LINKFLAGS=-Wl,-rpath,"$pathToPythonLib
+" LINKFLAGS=-Wl,-rpath="$pathToPythonLib
 
 install()
 {
