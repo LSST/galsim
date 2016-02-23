12bf5d912fdf951826452d1375f55066d6c5a037
Joshua Hoblitt <josh@hoblitt.com>
2016-02-23 08:51:13 -0700

    remove usage of DYLD_FALLBACK_LIBRARY_PATH
    + fix shellcheck warnings

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -1,15 +1,16 @@
-export SCONSFLAGS=$SCONSFLAGS" USE_UNKNOWN_VARS=true TMV_DIR="$TMV_DIR" PREFIX="$PREFIX" PYPREFIX="$PREFIX"/lib/python EXTRA_LIB_PATH="$TMV_DIR"/lib EXTRA_INCLUDE_PATH="$TMV_DIR"/include"
+#!/bin/bash
 
-pathToPython=$(which python)
-export DYLD_FALLBACK_LIBRARY_PATH="${pathToPython%bin/python}/lib"
+export SCONSFLAGS="${SCONSFLAGS} USE_UNKNOWN_VARS=true TMV_DIR=${TMV_DIR} PREFIX=${PREFIX} PYPREFIX=${PREFIX}/lib/python EXTRA_LIB_PATH=${TMV_DIR}/lib EXTRA_INCLUDE_PATH=${TMV_DIR}/include"
 
 install()
 {
     default_install
-    cp -r include $PREFIX/
+    cp -r include "$PREFIX"/
 
     if [[ $OSTYPE == darwin* ]]; then
-        install_name_tool -add_rpath $DYLD_FALLBACK_LIBRARY_PATH "$PREFIX"/lib/python/galsim/_galsim.so
+        local pathToPython
+        pathToPython=$(which python)
+        install_name_tool -add_rpath "${pathToPython%bin/python}/lib" "$PREFIX"/lib/python/galsim/_galsim.so
         install_name_tool -change libpython2.7.dylib @rpath/libpython2.7.dylib "$PREFIX"/lib/python/galsim/_galsim.so
     fi
 }
