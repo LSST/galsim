61ed1e626823572c2123cee9495562d57d840eee
danielsf <scott.f.daniel@gmail.com>
2016-03-29 15:11:07 -0700

    call scons explicitly with DYLD_FALLBACK_LIBRARY_PATH

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -3,6 +3,12 @@ export SCONSFLAGS=$SCONSFLAGS" USE_UNKNOWN_VARS=true TMV_DIR="$TMV_DIR" PREFIX="
 pathToPython=$(which python)
 export DYLD_FALLBACK_LIBRARY_PATH="${pathToPython%bin/python}/lib"
 
+build()
+{
+    scons DYLD_FALLBACK_LIBRARY_PATH=$DYLD_FALLBACK_LIBRARY_PATH -j$NJOBS prefix="$PREFIX" version="$VERSION" cc="$CC"
+
+}
+
 install()
 {
     default_install
